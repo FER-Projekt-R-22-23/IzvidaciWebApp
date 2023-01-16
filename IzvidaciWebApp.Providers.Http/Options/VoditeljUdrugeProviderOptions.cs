d1d892319a28163f2fd6c201ee4563d922dd7ee6
MartinaPavlovic <115044576+MartinaPavlovic@users.noreply.github.com>
2023-01-16 22:53:00 +0100

    VoditeljiUdruge

---
+++ IzvidaciWebApp.Providers.Http/Options/VoditeljUdrugeProviderOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzvidaciWebApp.Providers.Http.Options
+{
+    public class VoditeljUdrugeProviderOptions
+    {
+        public string BaseUrl { get; init; }
+    }
+}
